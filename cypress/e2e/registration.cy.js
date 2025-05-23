2a57b0179303f918dbe5659c950241e31eaac9c3
Yurii_Popovskiy <ronondex663@gmail.com>
2025-05-23 12:21:57 +0300

    AddTestForregistration

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -2,10 +2,30 @@
 
 describe('Student Registration page', () => {
   before(() => {
-
+    cy.visit('https://demoqa.com/automation-practice-form');
   });
 
-  it('', () => {
-
+  it('should fill all fields in refister forms', () => {
+    cy.get('#firstName').type('Carmelo');
+    cy.get('#lastName').type('Anthony');
+    cy.get('#userEmail').type('carmeloAnt663@gmail.com');
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(1)').click();
+    cy.get('#userNumber').type('0542094939');
+    cy.get('#dateOfBirthInput').click();
+    cy.get('.react-datepicker__month-select').select('July');
+    cy.get('.react-datepicker__year-select').select('1990');
+    cy.get('.react-datepicker__day--012').click();
+    cy.get('.subjects-auto-complete__value-container').type('Arts{Enter}');
+    // eslint-disable-next-line max-len
+    cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label')
+      .click();
+    cy.get('#currentAddress').type('Springfield, Wall Ave, 74');
+    cy.get('#stateCity-wrapper > :nth-child(2)').click();
+    cy.get('#react-select-3-option-0').type('NRC');
+    cy.get('.css-1wa3eu0-placeholder').click();
+    cy.get('#react-select-4-option-0').type('Delhi');
+    cy.get('#submit').click();
+    cy.get('.modal-header')
+      .should('contain.text', 'Thanks for submitting the form');
   });
 });
